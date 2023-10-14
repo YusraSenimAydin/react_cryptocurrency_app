14ef297cd254612b2b73c216899711c63e35b325
YusraSenimDede <50294199+YusraSenimAydin@users.noreply.github.com>
2023-10-15 00:01:15 +0300

    add store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    'tiers[0]': '1',
+    orderBy: 'marketCap',
+    orderDirection: 'desc',
+    limit: '50',
+    offset: '0'
+  },
+  headers: {
+    'X-RapidAPI-Key': 'ccdd7131c8mshfb1b82c53fc776bp139a56jsn9147b6e0833c',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
